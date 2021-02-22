d8842ae3459f1ace8606295361217068315264cf
Gosha <begoshaaaa@gmail.com>
2021-02-22 20:44:40 +0600

    TaskAdmin added

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from webapp.models import Task
 
-# Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+
+    list_display = ['id', 'status', 'created_at']
+
+    list_filter = ['status']
+
+    search_fields = ['status', 'completion_date']
+
+    fields = ['description', 'status', 'completion_date', 'created_at', 'updated_at']
+
+    readonly_fields = ['created_at', 'updated_at']
+
+admin.site.register(Task, TaskAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'),  ('done', 'Сделано')]
+class Task(models.Model):
+    description = models.CharField(max_length=1500, null=False, blank=False, verbose_name='Task Description')
+    status = models.CharField(max_length = 11, choices = status_choices, verbose_name='Choice')
+    completion_date = models.DateField(default=None, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
 
-# Create your models here.
+    def __str__(self):
+
+        return "{}. {}".format(self.pk, self.title)
