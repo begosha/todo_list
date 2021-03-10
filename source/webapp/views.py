8e0601cb11e47c89e8732534c2d25584255b39ff
Gosha <begoshaaaa@gmail.com>
2021-03-10 17:56:46 +0600

    task add view for get request: context with form is added to render

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from webapp.models import Task
 from django.urls import reverse
+from webapp.forms import TaskForm
 
 def index_view (request):
     tasks = Task.objects.all()
@@ -22,18 +23,47 @@ def task_view(request, pk):
         task.delete()
         return redirect('index')
 
-def task_add_view(request, *args, **kwargs):
+def task_add_view(request):
     if request.method == 'GET':
-        return render(request, 'task_add_view.html')
+        form = TaskForm()
+        return render(request, 'task_add_view.html', context={'form': form})
     elif request.method == 'POST':
-        description = request.POST.get('description')
-        details = request.POST.get('details')
-        status = request.POST.get('status')
-        completion_date = request.POST.get('completion_date')
-        task = Task.objects.create(description=description, details=details, status=status, completion_date=completion_date)
-        context = {
-            'task': task
-        }
-        url = reverse('task', kwargs={'pk': task.pk})
-        return redirect(url)
+        form = TaskForm(data=request.POST)  
+        if form.is_valid():  
+            task = Task.objects.create(
+                description=form.cleaned_data.get('description'),
+                details=form.cleaned_data.get('details'),
+                status=form.cleaned_data.get('status'),
+                completion_date=form.cleaned_data.get('completion_date')
+            )
+            return redirect('task', pk=task.id)  
+        return render(request, 'task_add_view.html', context={'form': form}) 
+
+        # description = request.POST.get('description')
+        # details = request.POST.get('details')
+        # status = request.POST.get('status')
+        # completion_date = request.POST.get('completion_date')
+        # task = Task.objects.create(description=description, details=details, status=status, completion_date=completion_date)
+        # context = {
+        #     'task': task
+        # }
+        # url = reverse('task', kwargs={'pk': task.pk})
+        # return redirect(url)
+
+def guest_add_view(request):
+    if request.method == "GET": 
+        form = GuestForm()
+        return render(request, 'guest_add_view.html', context={'form': form})
+    elif request.method == "POST": 
+        form = GuestForm(data=request.POST)  
+        if form.is_valid():  
+            guest = Guest.objects.create(
+                name=form.cleaned_data.get('name'),
+                email=form.cleaned_data.get('email'),
+                booking_details=form.cleaned_data.get('booking_details')
+            )
+            return redirect('guest', pk=guest.id)  
+        return render(request, 'guest_add_view.html', context={'form': form}) 
+
+
 
